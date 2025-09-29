f26c4b830a77f27aa819ef66f522ad049338c2c5
Karidus-423 <kapuerto23@gmail.com>
2025-09-29 12:21:46 -0400

    yipee, I get imports now

--- build.zig
+++ build.zig
@@ -6,11 +6,13 @@ pub fn build(b: *std.Build) void {
 
     const engine = b.addExecutable(.{
         .name = "daedalus",
-        .target = architecture,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .target = architecture,
+            .optimize = optimize,
+            .link_libc = true,
+        }),
     });
 
-    engine.linkLibC();
     engine.linkSystemLibrary("SDL3");
 
     engine.addCSourceFiles(.{
@@ -33,17 +35,27 @@ pub fn build(b: *std.Build) void {
     const run_engine_step = b.step("run", "Run Daedalus");
     run_engine_step.dependOn(&run_engine_cmd.step);
 
-    const tests_mod = b.createModule(.{
-        .root_source_file = b.path("tests/test_root.zig"),
-        .target = architecture,
-        .optimize = optimize,
-    });
-    
-    const unit_tests = b.addTest(.{
-        .root_module = tests_mod,
-    });
-
-    const run_unit_tests =  b.addRunArtifact(unit_tests);
+    const tests = [_][]const u8{
+        "tests/soupc/test_soup_strings.zig",
+    };
     const tests_step = b.step("test", "Run unit tests");
-    tests_step.dependOn(&run_unit_tests.step);
+
+    for (tests) |file| {
+        const tests_mod = b.createModule(.{
+            .root_source_file = b.path(file),
+            .target = architecture,
+            .optimize = optimize,
+            .link_libc = true,
+        });
+        tests_mod.addIncludePath(b.path("soupc"));
+
+        const unit_tests = b.addTest(.{
+            .root_module = tests_mod,
+        });
+        unit_tests.linkSystemLibrary("SDL3");
+
+        const run_unit_tests =  b.addRunArtifact(unit_tests);
+        tests_step.dependOn(&run_unit_tests.step);
+    }
+
 }
--- tests/soupc/test_soup_strings.zig
+++ tests/soupc/test_soup_strings.zig
@@ -1,10 +1,19 @@
 const std = @import("std");
+const mem = std.mem;
 const expect = std.testing.expect;
+
 const soup = @cImport({
     @cDefine("SOUPC_STRINGS_IMPLEMENTED", {});
-    @cInclude("../../src/soupc/soup-strings.h");
+    @cInclude("soup-strings.h");
 });
 
-test "Test RemoveSubstring"{
-    try expect();
+// char* RemoveSubString(const char* string,const char* sub);
+test "Test RemoveSubstring" {
+    const s = "ACABAD";
+    const sub = "CAB";
+
+    const to_slice = soup.RemoveSubString(s, sub);
+    const wrapped_res = mem.span(to_slice);
+
+    try expect(mem.eql(u8, wrapped_res, "ACD"));
 }
