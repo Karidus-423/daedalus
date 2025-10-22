9f2d726bec5ce2e4ab7c9e2906bddbab9dee6b29
Karidus-423 <kapuerto23@gmail.com>
2025-10-21 20:18:40 -0400

    String_Parse Test Passed

--- tests/engine/test_types.zig
+++ tests/engine/test_types.zig
@@ -10,32 +10,35 @@ fn Zig_StringGetSub(str: []const u8, start: u32, end: u32) []const u8 {
 }
 
 test "Test String_GetSub" {
-    const input : dt.String = .{ .chars = "examplestringedge",.len = 18 };
+    const input: dt.String = .{ .chars = "examplestringedge", .len = 18 };
     const in_ptr: [*c]const dt.String = @ptrCast(&input);
 
-    const actual: [*c] dt.String  = dt.String_GetSub(in_ptr, 7, 13);
+    const actual: [*c]dt.String = dt.String_GetSub(in_ptr, 7, 13);
     const expect = Zig_StringGetSub("examplestringedge", 7, 13);
 
     try testing.expect(mem.eql(u8, expect, mem.span(actual.*.chars)));
 }
 
 test "Test String_Parse" {
-    const input: dt.String = .{.chars = "filename.extension1.extension2", .len = 31};
+    const input: dt.String = .{ .chars = "filename.extension1.extension2", .len = 31 };
     const in_ptr: [*c]const dt.String = @ptrCast(&input);
 
-    const actual: [*c] dt.List = dt.String_Parse(in_ptr, '.');
+    var actual: [*c]dt.List = dt.String_Parse(in_ptr, '.');
 
     var node_num: i8 = 0;
+
+    //TODO: Find the expression to cast a void* as String*. Read translated.zig and translate.c to
+    //understand how to type cast the void*.
     while (actual.*.next != null) {
-        //TODO: Find the expression to cast a void* as String*. Read translated.zig and translate.c to
-        //understand how to type cast the void*.
-        const node_data_type: [*c]const dt.String = @ptrCast(@alignCast(actual.*.data));
+        actual = actual.*.next;
+        const node_data_type: [*c]const dt.String =  @ptrCast(@alignCast(actual.*.data));
         const actual_data: []const u8 = mem.span(node_data_type.*.chars);
+
         try switch (node_num) {
-            0 => testing.expect(mem.eql(u8,"filename", actual_data)),
-            1 => testing.expect(mem.eql(u8,"extension1", actual_data)),
-            2 => testing.expect(mem.eql(u8,"extension2", actual_data)),
-            else => testing.expect(false)
+            0 => testing.expect(mem.eql(u8, "extension2", actual_data)),
+            1 => testing.expect(mem.eql(u8, "extension1", actual_data)),
+            2 => testing.expect(mem.eql(u8, "filename", actual_data)),
+            else => testing.expect(false),
         };
         node_num += 1;
     }
